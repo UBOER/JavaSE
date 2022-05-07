c6b31df111f4f2829064ba72d0055086b5948202
liubo <liuboubo@qq.com>
2022-05-07 21:34:16 +0800

    第一节代码课 输出 helloword

---
+++ src/StudyJava/Test01.java
@@ -0,0 +1,17 @@
+package StudyJava;
+
+// Test001  必需跟文件名一模一样
+public class Test01 {
+
+    //使用快捷键，以后这个不写，主入口程序，千年不变的 ，不需要记，也不需要抄
+    //psvm 回车 或者main 回车
+
+    public static void main(String[] args) {
+        //打印输出信息  sout 回车
+        System.out.println("hello world");
+    }
+
+
+
+
+}
