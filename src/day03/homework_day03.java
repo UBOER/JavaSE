0e79ee27a19a87b1f6a8dd0a716eb4bc0f205445
liubo <liuboubo@qq.com>
2022-05-08 18:39:54 +0800

    作业： 输出商品价目表 运用链接输出和\t\n

---
+++ src/day03/homework_day03.java
@@ -0,0 +1,21 @@
+package day03;
+
+public class homework_day03 {
+    /**
+     * 在控制台输出商品价目表
+     */
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t购物清单");
+        System.out.println("商品名称\t\t购买数量\t\t商品单价\t\t金额");
+        System.out.println("鳄鱼夹克\t\t" +
+                "1\t\t\t" +
+                "1900\t\t" +
+                "1900");
+        System.out.println("iopdMP4\t\t" +
+                "1\t\t\t" +
+                "1900\t\t" +
+                "1900");
+
+    }
+}
