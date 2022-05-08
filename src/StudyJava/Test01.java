0f7c01d931ce4b5a42f7071c275627160a375f1f
liubo <liuboubo@qq.com>
2022-05-08 17:50:18 +0800

    这里主要是学习了基础的 注释、输出、制表符、换行符

--- src/StudyJava/Test01.java
+++ src/StudyJava/Test01.java
@@ -1,17 +1,35 @@
-package StudyJava;
+/**package  StudyJava;
 
-// Test001  必需跟文件名一模一样
-public class Test01 {
-
-    //使用快捷键，以后这个不写，主入口程序，千年不变的 ，不需要记，也不需要抄
-    //psvm 回车 或者main 回车
-
+class  Test01{
     public static void main(String[] args) {
-        //打印输出信息  sout 回车
-        System.out.println("hello world");
+        System.out.println("Hello word");
     }
+}*/
+//第一遍
+/**
+ * package StudyJava;
+ * class Test01{
+ *     public static void main(String[] args)
+ *     {
+ *         System.out.println("Hello word");
+ *     }
+ * }
+ */
+//第二遍
+/**
+ * package StudyJava;
+ * class Test01{
+ *     public static void main(String[] args)
+ *     {
+ *         System.out.println("Hello word");
+ *     }
+ * }
+ */
+//第三遍
 
-
-
-
+package  StudyJava;
+class Test01{
+    public static void main(String[] args) {
+        System.out.println("我爱学Java");
+    }
 }
---
+++ src/day03/demo01.java
@@ -0,0 +1,28 @@
+package day03;
+
+//第三遍
+class demo01{
+    /**public 公开类后续在oop会讲
+     * class类
+     * demo01 类名 必须与源文件名一致
+     */
+    public static void main(String[] args) {
+        //单行注释
+        /*
+        多行注释
+        要养成写注释的好习惯
+         */
+        /**
+         * 这是文档注释，通常用于方法或类前
+         * 以后课程需要用到
+         */
+        System.out.println(123);//打印数字不需要""''
+        System.out.println("我爱学java，我要跟着曾老师学java，高薪就业1");
+        System.out.println("我爱学   java，我要跟着曾老师学java，高薪就业2");
+
+        // /t必须在""中，切必须是/
+
+        System.out.println("我爱学\tjava，我要跟着曾老师学java，高薪就业3");
+        System.out.println("我爱学\njava，我要跟着曾老师学java，高薪就业4");
+    }
+}
