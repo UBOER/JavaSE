73672044df97be2fb32ea44770a0d64287964005
liubo <liuboubo@qq.com>
2022-05-08 18:56:39 +0800

    作业：
         *使用变量存储以下MP3信息，并打印输出
         * 品牌 (brand)：爱国者F928
         * 重量 (weight)：12.4
         * 电池类型（type)：内置锂电池
         * 价格 (price)： 499
    
    巩固\t \n 还有连接输出
    判断选择何种变量类型存储相应的变量

---
+++ src/day03/homework_day03_3.java
@@ -0,0 +1,22 @@
+package day03;
+
+public class homework_day03_3 {
+    /**
+     *使用变量存储以下MP3信息，并打印输出
+     * 品牌 (brand)：爱国者F928
+     * 重量 (weight)：12.4
+     * 电池类型（type)：内置锂电池
+     * 价格 (price)： 499
+     */
+
+    public static void main(String[] args) {
+        String brand="爱国者F928";//字符串类型
+        double weight=12.4;//double类型
+        String type="内置锂电池";//字符串
+        int price=499;//整型
+
+        System.out.println("品牌 (brand)："+brand+"\n" +"重量 (weight)："+weight+"\n" +"电池类型（type)："+
+                type+"\n"+"价格 (price)："+price);
+
+    }
+}
