a87ab6f26ebb7a51d9a39a8d4e1240c93cbb81cd
liubo <liuboubo@qq.com>
2022-05-08 18:50:09 +0800

    作业： 在控制台输出登录系统菜单 和 系统主菜单
    巩固\t \n 还有输出

---
+++ src/day03/homework_day03_2.java
@@ -0,0 +1,34 @@
+package day03;
+
+public class homework_day03_2 {
+    /**
+     * 在控制台输出购物系统登录菜单和系统主菜单
+     */
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t欢迎使用我行我素购物管理系统");//登录系统菜单
+        System.out.println("");
+        System.out.println("\t\t\t\t\t\t1.登 录 系 统");
+        System.out.println("\n");
+        System.out.println("\t\t\t\t\t\t2.退 出");
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.out.println("请选择，输入数字：");
+
+
+        System.out.println("\n\n\n\n");
+
+
+        System.out.println("\t\t\t欢迎使用我行我素购物管理系统");//系统主菜单
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.out.println("");
+        System.out.println("\t\t\t\t\t\t1.客 户 信 息 管 理");
+        System.out.println("\n");
+        System.out.println("\t\t\t\t\t\t2.购 物 结 算");
+        System.out.println("\n");
+        System.out.println("\t\t\t\t\t\t3.真 情 回 馈");
+        System.out.println("\n");
+        System.out.println("\t\t\t\t\t\t4.注 销");
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.out.println("请选择，输入数字：");
+    }
+}
