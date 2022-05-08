c7a8f8865304e45c6682dd25320cdd786ca17591
liubo <liuboubo@qq.com>
2022-05-08 20:57:06 +0800

    学习了：接收输入值

---
+++ src/day04/demo01.java
@@ -0,0 +1,19 @@
+package  day04;
+class demo01{
+    public static void main(String[] args) {
+        System.out.println();
+
+        System.out.println(32);
+        /* if (){}
+            int a=1;
+            }else{
+            }
+         *///注意缩进
+        int num=10;
+        int num2;
+        //num2=num;
+        // System.out.println(num2);
+        // num=num2;//num2没有值
+        //第三遍
+    }
+}
