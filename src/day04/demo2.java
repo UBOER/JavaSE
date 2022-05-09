e93ba8a1440c762785ed5951eb28231b00992966
liubo <liuboubo@qq.com>
2022-05-09 11:16:04 +0800

    学习了：自+ 自减运算符

--- src/day04/demo2.java
+++ src/day04/demo2.java
@@ -2,28 +2,22 @@ package day04;
 
 import java.util.Scanner;
 
-public class demo2 {
+class demo2{
     public static void main(String[] args) {
-        //接受我们输入的值，这个以后开发过程中不用，只是目前学习，需要使用到。
-        //这一个等到学了oop自然懂了
-
-        Scanner  sc=new Scanner(System.in);
-
-        System.out.println("请输入一个数字，int类型，前面先定义一个int的变量");
+        //接收我们输入的值，这个以后开发不用
+        Scanner sc= new Scanner(System.in);
+        System.out.println("请输入一个数字，int，前面先定义一个Int的变量");
         int num=sc.nextInt();
+        System.out.println("接收到的int的值为"+num);
 
-
-        System.out.println("接受到的int值为："+num);
-
-        System.out.println("请输入一个double类型数据");
-
-        double num2= sc.nextDouble();
-
-        System.out.println("接受到的double的值为"+num2);
-
+        System.out.println("请输入一个double数据");
+        double num2=sc.nextDouble();
+        System.out.println("该double的值为" + num2);
 
         System.out.println("请输入一个字符串");
         String str=sc.next();
 
+        System.out.println("该字符串的值为：" +str);
+        //第二遍
     }
 }
--- src/day04/demo5.java
+++ src/day04/demo5.java
@@ -4,10 +4,23 @@ public class demo5 {
 
     public static void main(String[] args) {
         int i=5;
-        System.out.println("i第一次的值为" + i);
+        System.out.println("i第一次的值为" + i);//此时i为5
         i++;
-        System.out.println("i第二次的值为"+i);
+        System.out.println("i第二次的值为"+i);//此时i为6
         i++;
+        System.out.println("i第三次的值为"+i);//此时i为7
+        int num=8;
+        num+=1;//相当于 num+1=9 //开发会用
+        System.out.println("num的值为"+num);
+
+        num+=6;
+        System.out.println("此时num的值为"+num);
+
+        int num2=20;
+        num2-=4; // 20- 4= 16
+        System.out.println(num2);
+
+
 
     }
 }
