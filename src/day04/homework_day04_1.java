386ed779b6bd091420ac57ed26f929d9b8535393
liubo <liuboubo@qq.com>
2022-05-09 17:15:18 +0800

    作业：完成购物小票 注意代码要求，定义变量，不写死等等

--- src/day04/homework_day04_1.java
+++ src/day04/homework_day04_1.java
@@ -20,7 +20,7 @@ public class homework_day04_1 {
         int count_Racket;
         int total_Racket;
 
-        tShirt="T恤";//给T恤赋值
+        tShirt = "T恤";//给T恤赋值
         price_tShirt = 245;
         count_tShirt = 2;
         total_tShirt = price_tShirt * count_tShirt;
@@ -40,15 +40,15 @@ public class homework_day04_1 {
         totalprice = (double) ((total_Racket + total_tShirt + total_Shoes) * 0.8);
 
         //定义实际缴费，这里写死
-        int real_pay_money=1500;
+        int real_pay_money = 1500;
 
         //定义找零
         double back_customer_money;
-        back_customer_money=real_pay_money-totalprice;
+        back_customer_money = real_pay_money - totalprice;
 
         //定义积分
         int point;
-        point = (int )totalprice/33;//商场积分都是按照 达到多少来积分的，由题意知道这里只能为33
+        point = (int) totalprice / 33;//商场积分都是按照 达到多少来积分的，由题意知道这里只能为33
 
 
         /*
@@ -56,15 +56,15 @@ public class homework_day04_1 {
          */
         System.out.println("************消费单************");
         System.out.println("购买物品\t\t单价\t\t个数\t\t金额\t");
-        System.out.println(tShirt+"\t\t\t"+price_tShirt+"\t\t"+count_tShirt+"\t\t"+total_tShirt);
-        System.out.println(shoes+"\t\t"+price_Shoes+"\t\t"+count_Shoes+"\t\t"+total_Shoes);
-        System.out.println(racket+"\t\t"+price_Racket+"\t\t"+count_Racket+"\t\t"+total_Racket);
+        System.out.println(tShirt + "\t\t\t" + price_tShirt + "\t\t" + count_tShirt + "\t\t" + total_tShirt);
+        System.out.println(shoes + "\t\t" + price_Shoes + "\t\t" + count_Shoes + "\t\t" + total_Shoes);
+        System.out.println(racket + "\t\t" + price_Racket + "\t\t" + count_Racket + "\t\t" + total_Racket);
         System.out.println("\n");
         System.out.println("折扣：\t\t8折");
-        System.out.println("消费总金额\t¥"+totalprice);
-        System.out.println("实际交费\t\t¥"+real_pay_money);
-        System.out.println("找钱\t\t\t¥"+back_customer_money);
-        System.out.println("本次购物所获的积分是："+point);
+        System.out.println("消费总金额\t¥" + totalprice);
+        System.out.println("实际交费\t\t¥" + real_pay_money);
+        System.out.println("找钱\t\t\t¥" + back_customer_money);
+        System.out.println("本次购物所获的积分是：" + point);
 
 
     }
