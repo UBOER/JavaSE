7b5acc7be2b993c87ae509760a1712073ddc0380
liubo <liuboubo@qq.com>
2022-05-10 19:43:17 +0800

    作业： 输入小明成绩 判断所获的奖励 if 、 else if、 else 这三个判断条件 得到运用

--- src/day06/homework_day06_5.java
+++ src/day06/homework_day06_5.java
@@ -1,4 +1,33 @@
 package day06;
 
+import java.util.Scanner;
+
+/**
+ * 输入小明的考试成绩，显示所获奖励
+ * 成绩==100分，爸爸给他买辆车
+ * 100分>成绩>=90分，妈妈给他买MP4
+ * 90分>成绩>=60分，妈妈给他买本参考书
+ * 成绩<60分，什么都不买
+ */
 public class homework_day06_5 {
+    public static void main(String[] args) {
+        int score;//成绩
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入小明的考试成绩！");
+        score = sc.nextInt();
+
+        if (score == 100) {
+            System.out.println("爸爸给他买辆车");
+        } else if (score >= 60 && score < 100) {
+            System.out.println("妈妈给他买MP4");
+        } else if (score >= 60 && score < 90) {
+            System.out.println("妈妈给他买本参考书");
+        } else if (score>100) {
+            System.out.println("考试成绩输入错误，请重新输入！(0-100)");
+        } else {
+            System.out.println("什么都不买");
+        }
+
+    }
+
 }
