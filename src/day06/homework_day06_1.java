7c73c603b7a1fc27b3d8a50476f3d97434659a9e
liubo <liuboubo@qq.com>
2022-05-10 17:19:03 +0800

    作业：买彩票（就是训练if else 语句..

---
+++ src/day06/homework_day06_1.java
@@ -0,0 +1,26 @@
+package day06;
+
+/**
+ * 买彩票
+ * 如果体彩中了500万，我买车、资助希望工程、去
+ * 欧洲旅游
+ * 如果没中，我买下一期体彩，继续烧高香
+ */
+public class homework_day06_1 {
+    public static void main(String[] args) {
+        String answer;//定义彩票是否中奖
+        answer = "中奖";
+        if (answer.equals("中奖")) {
+            System.out.println("我买车、资助希望工程、去欧洲旅游!");
+        } else {
+            System.out.println("我买下一期体彩，继续烧高香");
+        }
+
+        answer = "没中";
+        if (answer.equals("中奖")) {
+            System.out.println("我买车、资助希望工程、去欧洲旅游!");
+        } else {
+            System.out.println("我买下一期体彩，继续烧高香");
+        }
+    }
+}
