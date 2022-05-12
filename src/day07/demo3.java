6aeb37f486ea3c54f0b22b0fa94a7d152dbd920b
liubo <liuboubo@qq.com>
2022-05-12 11:55:27 +0800

    学写了：运用swich来判断,这个demo主要展示大概语法，下一个demo来讲解需要注意的地方。

--- src/day07/demo3.java
+++ src/day07/demo3.java
@@ -9,22 +9,22 @@ public class demo3 {
          * 如果获得第三名，将奖励移动硬盘一个
          * 否则，不给任何奖励
          *
-         * 运用swich 完成
+         * 运用switch 完成
          */
 
         int num=3;
-        switch (num) {
-            case 1:
+        switch (num) {        // switch后面的数字是与下面的 case进行判断，如果相等就进入下一个条件
+            case 1: //相当于if else
                 System.out.println("奖励一个月夏令营");
-                break;
-            case 2:
+                break;      //判断完要写break 进入后会执行下面case2 的代码
+            case  2:
                 System.out.println("奖励惠普笔记本电脑一部");
                 break;
 
             case 3:
                 System.out.println("奖励移动硬盘");
                 break;
-            default:
+            default:   //就相当于else
                 System.out.println("啥都没有");
                
 
