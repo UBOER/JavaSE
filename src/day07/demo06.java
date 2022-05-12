e26f133d3b3512aa9c584bb1d5ff75e7e1abcab9
liubo <liuboubo@qq.com>
2022-05-12 12:54:29 +0800

    学习了：关于switch可使用的数据类型，复习了数据类型。

---
+++ src/day07/demo06.java
@@ -0,0 +1,21 @@
+package day07;
+
+public class demo06 {
+    public static void main(String[] args) {
+        /**
+         * switch里面 能加什么？
+         * 不止是int类型
+         * 可以使用的基本数据类型：
+         * byte short char ->可以转化成 int
+         * 引用数据类型：枚举 Enum
+         *String jdk1.7以后就可以
+         *
+         * 基本数据类型:int short long byte
+         * double float
+         * char
+         * boolean
+         *
+         */
+
+    }
+}
