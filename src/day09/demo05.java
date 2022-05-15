b938e3160736dfe4fd6b6a1d7ecd065cba86cfbb
liubo <liuboubo@qq.com>
2022-05-15 19:52:45 +0800

    学习了：一点点数组的知识

---
+++ src/day09/demo05.java
@@ -0,0 +1,42 @@
+package day09;
+
+public class demo05 {
+    /**
+     * 数组，实际开发中百分之百不会用二维数组
+     * 都是一维数组
+     */
+
+    public static void main(String[] args) {
+        //定义变量：
+        int num=10;//一个变量只能赋一个值，能不能一个变量赋多个值？
+
+        /**
+         * 使用数组 就可以解决这个问题。
+         * 我们讲数组就是为了后面更好的去讲集合
+         */
+
+        /**
+         * 创建数组
+         */
+       /* int[] nums = {10, 20, 30, 40,50};
+        //对应下标：    0,  1   ,2  ,3, 4
+        System.out.println(nums.length);
+        //挨着输出
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(nums[i]);
+        }*/
+
+        /**
+         * 能不能输出nums[5]
+         *  System.out.println(nums[5])
+         *  不可以。报错数组越界
+         *
+         *
+         *  并且可以修改数组的值
+         */
+
+
+
+
+    }
+}
