a8542aa733544a7fe0458a6476699decc9248076
liubo <liuboubo@qq.com>
2022-05-15 19:30:16 +0800

    学习了：for循环弄一个加法表

---
+++ src/day09/demo03.java
@@ -0,0 +1,15 @@
+package day09;
+
+public class demo03 {
+    public static void main(String[] args) {
+        /**
+         * 打印加法表
+         */
+        int val = 6;
+        for (int i = 0, j = val; i <= 6; i++, j--) {
+            System.out.println(i+"+"+j+"= "+(i + j));
+        }
+
+
+    }
+}
